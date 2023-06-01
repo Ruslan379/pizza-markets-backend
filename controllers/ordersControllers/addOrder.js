d95ed9bd9bc69aeb895667fadf822aeed381b7d7
Ruslan379 <arnov@i.ua>
2023-06-02 02:26:01 +0300

    add ordersRouter-2

--- controllers/ordersControllers/addOrder.js
+++ controllers/ordersControllers/addOrder.js
@@ -6,23 +6,24 @@ const { lineBreak } = require("../../services");
 
 //-----------------------------------------------------------------------------
 const addOrder = async (req, res, next) => {
-    // const order = await Order.create(req.body);
+    console.log("addOrder-->req.body:".bgYellow.red); //?
+    console.log(req.body);
+    const order = await Order.create(req.body);
 
-    const { id: userId } = req.user //?
+
+    // const { id: userId } = req.user //?
     //* =============================console===================================
-    console.log("addOrder-->req.user:".bgYellow.red); //?
-    // console.table(req.user); //?
-    // console.table([req.user]);
-    console.log(req.user);
+    // console.log("addOrder-->req.user:".bgYellow.red); //?
+    // // console.table(req.user); //?
+    // // console.table([req.user]);
+    // console.log(req.user);
 
-    console.log("addOrder-->userId:".bgYellow.blue, userId.bgGreen.blue); //?
-    console.log("");
+    // console.log("addOrder-->userId:".bgYellow.blue, userId.bgGreen.blue); //?
+    // console.log("");
     //* =======================================================================
 
     //! С добавлением _id Пользователя, который сделал заказ
     // const order = await Order.create({ ...req.body, owner: userId }); //?
-
-    const order = await Order.create(req.body); //?
 
 
     //! ===========================console============================
@@ -37,12 +38,7 @@ const addOrder = async (req, res, next) => {
     //! ==============================================================
 
 
-    res.status(201).json({
-        status: "success",
-        code: 201,
-        // data: { order }, //??
-        order,
-    });
+    res.status(201).json({ order });
 };
 
 module.exports = addOrder;
